5d8fab76d6b3861c5f4fa35cb615099d36664af5
Frank Marazita <fmarazita1@gmail.com>
2025-04-09 02:19:55 +1000

    fixed deploy issues

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ source .env
 
 REPO_URL="$(git config --get remote.origin.url)"
 REPO_USER=$(echo "$REPO_URL" | sed -E 's/.*[:\/]([^\/]+)\/.*/\1/')
-REPO_NAME=$(echo "$REPO_URL" | sed -E 's/.*[:\/]([^\/]+)\/(.*)\.git/\2/')
+export REPO_NAME=$(echo "$REPO_URL" | sed -E 's/.*[:\/]([^\/]+)\/(.*)\.git/\2/')
 
 echo $GITHUB_TOKEN | docker login ghcr.io -u $REPO_USER --password-stdin
 
-IMAGE_URL="ghcr.io/$REPO_USER/$REPO_NAME"
+export IMAGE_URL="ghcr.io/$REPO_USER/$REPO_NAME"
 
 docker pull $IMAGE_URL:latest
 
