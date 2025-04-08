e53bb450f0b03b0e6f993670a242bcd298ad6cbb
Frank Marazita <fmarazita1@gmail.com>
2025-04-09 01:15:23 +1000

    added messaging to ci script

--- ci.sh
+++ ci.sh
@@ -4,4 +4,20 @@ set -eu -o pipefail
 
 git pull
 
-./deploy.sh
+./deploy.sh || DEPLOY_FAILED=true
+
+REPO_URL="$(git config --get remote.origin.url)"
+REPO_NAME=$(echo "$REPO_URL" | sed -E 's/.*[:\/]([^\/]+)\/(.*)\.git/\2/')
+
+TITLE="Deploy: $REPO_NAME"
+BODY="Deployed successfully ✅"
+
+if [ "$DEPLOY_FAILED" = true ]; then
+  BODY="Failed to deploy ❌"
+fi
+
+curl -X POST -H "Content-Type: application/json" -d '{ "type": "discord", "title": "'"$TITLE"'", "body": "'"$BODY"'" }' http://localhost:7665/message || true
+
+if [ "$DEPLOY_FAILED" = true ]; then
+  exit 1
+fi
